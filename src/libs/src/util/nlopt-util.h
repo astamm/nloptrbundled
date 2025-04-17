22c877fb02c21053c4d0c14973ee2c1088820dc2
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2025-04-17 20:37:24 +0200

    Move defines in nlopt CMakeLists.

--- src/libs/src/util/nlopt-util.h
+++ src/libs/src/util/nlopt-util.h
@@ -30,7 +30,7 @@
 
 #include "nlopt.h"
 
-#define CRAN_COMPATIBILITY
+//#define CRAN_COMPATIBILITY
 
 /* workaround for Solaris + gcc 3.4.x bug (see configure.ac) */
 #if defined(__GNUC__) && defined(REPLACEMENT_HUGE_VAL)
